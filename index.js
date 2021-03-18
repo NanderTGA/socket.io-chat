42ffabdf57512a7f856727fff856a8d8e114be98
NanderTGA <nander.roobaert@gmail.com>
2021-03-18 06:29:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/client/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("someone connected");
+  io.emit("user joined")
+  socket.on("disconnect", () => {
+    console.log("someone disconnected");
+    io.emit("user left")
+  });
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+app.use("/client", express.static(__dirname + "/client"));
+
+httpServer.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
